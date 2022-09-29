a37ed5decf7ee943a81d897bebf95cdf36a1d2ea
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-09-29 06:02:12 +0000

    Configure NPM package name + imports

---
+++ src/bicep-types/src/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+export * from "./writers/json";
+export * from "./writers/markdown";
+export * from "./indexer";
+export * from "./types";
