81f0b31721da518b8c4861d5c67f1c91475dfc81
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-10-13 15:33:21 -0400

    Fix handling of undefined type (#25)

--- src/bicep-types/test/integration/types.test.ts
+++ src/bicep-types/test/integration/types.test.ts
@@ -14,11 +14,13 @@ describe('types tests', () => {
       abc: { Type: factory.builtInTypes[BuiltInTypeKind.String], Flags: ObjectPropertyFlags.None, Description: 'Abc prop' },
       def: { Type: factory.builtInTypes[BuiltInTypeKind.Object], Flags: ObjectPropertyFlags.ReadOnly, Description: 'Def prop' },
       ghi: { Type: factory.builtInTypes[BuiltInTypeKind.Bool], Flags: ObjectPropertyFlags.WriteOnly, Description: 'Ghi prop' },
+      dictType: { Type: factory.addObjectType('dictType', {}, factory.builtInTypes[BuiltInTypeKind.Any]), Flags: ObjectPropertyFlags.None, Description: 'Dictionary of any' },
+      arrayType: { Type: factory.addArrayType(factory.builtInTypes[BuiltInTypeKind.Any]), Flags: ObjectPropertyFlags.None, Description: 'Array of any' },
     });
     const res = factory.addResourceType('foo@v1', ScopeType.Unknown, undefined, props, ResourceFlags.None);
 
     const json = writeJson(factory.types);
-    expect(json).toBe('[{\"1\":{\"Kind\":1}},{\"1\":{\"Kind\":2}},{\"1\":{\"Kind\":3}},{\"1\":{\"Kind\":4}},{\"1\":{\"Kind\":5}},{\"1\":{\"Kind\":6}},{\"1\":{\"Kind\":7}},{\"1\":{\"Kind\":8}},{\"2\":{\"Name\":\"foo\",\"Properties\":{\"abc\":{\"Type\":4,\"Flags\":0,\"Description\":\"Abc prop\"},\"def\":{\"Type\":5,\"Flags\":2,\"Description\":\"Def prop\"},\"ghi\":{\"Type\":2,\"Flags\":4,\"Description\":\"Ghi prop\"}}}},{\"4\":{\"Name\":\"foo@v1\",\"ScopeType\":0,\"Body\":8,\"Flags\":0}}]');
+    expect(json).toBe("[{\"1\":{\"Kind\":1}},{\"1\":{\"Kind\":2}},{\"1\":{\"Kind\":3}},{\"1\":{\"Kind\":4}},{\"1\":{\"Kind\":5}},{\"1\":{\"Kind\":6}},{\"1\":{\"Kind\":7}},{\"1\":{\"Kind\":8}},{\"2\":{\"Name\":\"dictType\",\"Properties\":{},\"AdditionalProperties\":0}},{\"3\":{\"ItemType\":0}},{\"2\":{\"Name\":\"foo\",\"Properties\":{\"abc\":{\"Type\":4,\"Flags\":0,\"Description\":\"Abc prop\"},\"def\":{\"Type\":5,\"Flags\":2,\"Description\":\"Def prop\"},\"ghi\":{\"Type\":2,\"Flags\":4,\"Description\":\"Ghi prop\"},\"dictType\":{\"Type\":8,\"Flags\":0,\"Description\":\"Dictionary of any\"},\"arrayType\":{\"Type\":9,\"Flags\":0,\"Description\":\"Array of any\"}}}},{\"4\":{\"Name\":\"foo@v1\",\"ScopeType\":0,\"Body\":10,\"Flags\":0}}]");
 
     const deserializedTypes = readJson(json);
     expect(deserializedTypes).toEqual(factory.types);
@@ -30,8 +32,15 @@ describe('types tests', () => {
 * **Valid Scope(s)**: Unknown
 ### Properties
 * **abc**: string: Abc prop
+* **arrayType**: any[]: Array of any
 * **def**: object (ReadOnly): Def prop
+* **dictType**: [dictType](#dicttype): Dictionary of any
 * **ghi**: bool (WriteOnly): Ghi prop
+
+## dictType
+### Properties
+### Additional Properties
+* **Additional Properties Type**: any
 
 `);
 
@@ -41,7 +50,7 @@ const index = buildIndex([{
 }], _ => {});
 
 const jsonIndex = writeIndexJson(index);
-expect(jsonIndex).toBe("{\"Resources\":{\"foo@v1\":{\"RelativePath\":\"foo/types.json\",\"Index\":9}},\"Functions\":{}}");
+expect(jsonIndex).toBe("{\"Resources\":{\"foo@v1\":{\"RelativePath\":\"foo/types.json\",\"Index\":11}},\"Functions\":{}}");
 
 const markdownIndex = writeIndexMarkdown(index);
 expect(markdownIndex).toBe(`# Bicep Types
