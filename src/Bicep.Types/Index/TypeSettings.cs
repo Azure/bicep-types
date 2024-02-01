c48c33e734a45f2d23ac98396df7e6ce6f99fd9b
Harsh Patel (from Dev Box) <harshpatel@microsoft.com>
2024-02-01 15:23:03 -0500

    Fixed errors

--- src/Bicep.Types/Index/TypeSettings.cs
+++ src/Bicep.Types/Index/TypeSettings.cs
@@ -9,8 +9,8 @@ namespace Azure.Bicep.Types.Index
     public class TypeSettings
     {
         [JsonConstructor]
-        public TypeSettings(string name, string version, bool isSingleton, TypeLocation configurationType)
-            => (Name, Version, IsSingleton, Configuration) = (name, version, isSingleton, configurationType);
+        public TypeSettings(string name, string version, bool isSingleton, TypeLocation configuration)
+            => (Name, Version, IsSingleton, Configuration) = (name, version, isSingleton, configuration);
 
         public string Name { get; }
 
