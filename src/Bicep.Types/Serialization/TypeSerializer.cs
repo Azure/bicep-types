701e6b5d5a4965aebbfd142868d10e9abe89b23a
Anu Thomas <anuamd@hotmail.com>
2025-06-25 17:48:54 -0700

    [AOT compliance] Using JsonSerializer.Deserialize overload that takes JsonTypeInfo (#696)
    * Add IL2026 suppression for the cases where JsonSerializerContext defines required type metadata
    
    * using JsonSerializer.Deserialize overload that takes JsonTypeInfo

--- src/Bicep.Types/Serialization/TypeSerializer.cs
+++ src/Bicep.Types/Serialization/TypeSerializer.cs
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using Azure.Bicep.Types.Concrete;
 using Azure.Bicep.Types.Index;
 
@@ -39,9 +38,10 @@ public static class TypeSerializer
     {
         var factory = new TypeFactory(Enumerable.Empty<TypeBase>());
         var options = GetSerializerOptions(factory);
+        // 'jsonTypeInfo' will have the original 'options' attached along with the type info for TypeBase[].
+        var jsonTypeInfo = (JsonTypeInfo<TypeBase[]>) options.GetTypeInfo(typeof(TypeBase[]));
 
-        var types = JsonSerializer.Deserialize<TypeBase[]>(contentStream, options)
-            ?? throw new JsonException("Failed to deserialize content");
+        var types = JsonSerializer.Deserialize(contentStream, jsonTypeInfo) ?? throw new JsonException("Failed to deserialize content");
 
         factory.Hydrate(types);
 
@@ -51,9 +51,9 @@ public static class TypeSerializer
     public static TypeIndex DeserializeIndex(Stream contentStream)
     {
         var options = GetSerializerOptions();
+        var jsonTypeInfo = (JsonTypeInfo<TypeIndex>) options.GetTypeInfo(typeof(TypeIndex));
 
-        return JsonSerializer.Deserialize<TypeIndex>(contentStream, options)
-            ?? throw new JsonException("Failed to deserialize index");
+        return JsonSerializer.Deserialize(contentStream, jsonTypeInfo) ?? throw new JsonException("Failed to deserialize index");
     }
 
     public static void SerializeIndex(Stream stream, TypeIndex index)
